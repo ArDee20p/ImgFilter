f4d5aed8d768f23127fd4ec1a39a44e381d59784
James Lord <caburb@gmail.com>
2022-04-09 11:50:32 -0700

    - refactor to frontend and backend division - divide code more regularly

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+import pymongo
+from pymongo.server_api import ServerApi
+
+
+client = pymongo.MongoClient(
+    "mongodb+srv://jlord:M0ng0DB@cluster0.lcjq9.mongodb.net/UserInfo?retryWrites=true&w=majority",
+    server_api=ServerApi('1'))
+db = client.LoginInfo
+coll = db.UserData
---
+++ frontend/login.py
@@ -0,0 +1,5 @@
+from __main__ import app
+
+@app.route('/login')
+def login():
+    return 'Hello World!'
