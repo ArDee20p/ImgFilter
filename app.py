d79fc5c5eba115e2950e57b89a6fbcca8c4dd656
James Lord <caburb@gmail.com>
2022-04-09 15:32:58 -0700

    - refactor to frontend and backend division - divide code more regularly

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import argparse
 from flask import Flask
 from pywebio.platform.flask import start_server
+import frontend.register as register
+
 
 app = Flask(__name__)
-
-import register
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
