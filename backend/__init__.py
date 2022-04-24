6e8478d55c4738eae5530bd1f993a3171fdb7efe
James Lord <caburb@gmail.com>
2022-04-24 09:48:18 -0700

    - working on user change info function

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 app.secret_key = 'a0bgF^P(m*jy^iQ$jntTCxLp)raqqpAG'
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 client = pymongo.MongoClient(
     "mongodb+srv://jlord:M0ng0DB@cluster0.lcjq9.mongodb.net/UserInfo?retryWrites=true&w=majority",
     server_api=ServerApi('1'))
+
+# login info collection
 db = client.get_database('LoginInfo')
 coll = db.UserData
+
+# image collection
+imagedb = client.get_database('ImageInfo')
+imagecoll = imagedb.ImageData
 
 from backend import routes
--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,9 @@
 import re
 import flask
 import flask_login
-from flask_login import current_user, login_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from passlib.handlers.pbkdf2 import pbkdf2_sha256
-from backend.models import User
+from backend.models import User as User
 from flask import request, redirect, render_template, url_for, flash, session
 from backend import app, coll, login_manager, db
 
@@ -13,23 +13,23 @@ usernameRegex = "^[a-zA-Z0-9äöüÄÖÜ]*$"
 
 @login_manager.user_loader
 def user_loader(email):
-    if not coll.find({"email": email}):
+    userfind = coll.find_one({"email": email})
+    if not userfind:
         return
 
-    user = User()
-    user.id = email
+    user = User(userfind['username'], userfind['email'], userfind['password'])
     return user
-#
-#
-# @login_manager.request_loader
-# def request_loader(request):
-#     email = request.form.get('email')
-#     if not coll.find({"email": email}):
-#         return
-#
-#     user = User()
-#     user.id = email
-#     return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    userfind = coll.find_one({"email": email})
+    if not userfind:
+        return
+
+    user = User(userfind['username'], userfind['email'], userfind['password'])
+    return user
 
 
 def check_inputs(credentials):
@@ -78,7 +78,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     message = 'Please login to your account'
-    if "email" in session:
+    if current_user.is_authenticated:
         return redirect(url_for("gallery"))
 
     if request.method == "POST":
@@ -91,10 +91,11 @@ def login():
             passwordcheck = email_found['password']
 
             if pbkdf2_sha256.verify(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
+                user = User(email_found['username'], email_val, passwordcheck)
+                login_user(user)  # TODO: create cookie to actually make this mean something.
                 return redirect(url_for('gallery'))
             else:
-                if "email" in session:
+                if current_user.is_authenticated:
                     return redirect(url_for("gallery"))
                 message = 'Wrong password'
                 return render_template('login.html', message=message)
@@ -106,22 +107,21 @@ def login():
 
 @app.route('/logout')
 def logout():
-    flask_login.logout_user()
-    session.pop('email', default=None)
-    return 'You have been logged out.'
+    logout_user()
+    return render_template('login.html', message='You have been logged out.')
 
 
 # TODO: create image editing frontend code here
 @app.route('/image_editor')
 def image_editor():
-    return 'Hello from Image Editor!'
+    return render_template('editor.html')
 
 
 # TODO: create public image gallery code here
 @app.route('/gallery')
 @login_required
 def gallery():
-    return 'Hello from Gallery!'
+    return render_template('gallery.html')
 
 
 @app.route('/user/<username>')
@@ -133,3 +133,29 @@ def profile(username):
         return redirect(url_for('gallery'))
     userimages = coll.find(uploader=user["username"])
     return render_template('user.html', user=user, userimages=userimages)
+
+
+@app.route('/change', methods=['GET', 'POST'])
+@login_required
+def changeinfo(username):
+    message = ''
+    if request.method == "POST":
+        email = request.form.get("email")
+
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        currpword = request.form.get("currentpassword")
+
+        user = coll.find_one({"email": email})
+
+        if not pbkdf2_sha256.verify(currpword, user["password"]):
+            message = 'Please enter your current password to change your login info!'
+            return render_template('change.html', message=message)
+        if password1 != password2:
+            message = 'Passwords must match!'
+            return render_template('change.html', message=message)
+        else:
+            hashed = pbkdf2_sha256.hash(password2.encode('utf-8'))
+            coll.updateOne({'username': username, 'email': email, 'password': hashed})
+            return render_template('login.html')
+    return render_template('login.html')
