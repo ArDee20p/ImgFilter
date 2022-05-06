f88825e3aef474916a756f1e172226a0ddc60f58
James Lord <caburb@gmail.com>
2022-05-06 13:02:37 -0700

    - functioning-ish gallery.html

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,13 +12,13 @@ ALLOWED_EXTENSIONS = {'png', 'jpeg', 'jpg'}
 INPUT_FILENAME = ''
 
 
-app = Flask(__name__, template_folder='./templates', static_folder='./static')
+app = Flask(__name__, template_folder='./templates')
 app.config['MONGODB_SETTINGS'] = {
     'host': "mongodb+srv://jlord:M0ng0DB@cluster0.lcjq9.mongodb.net/UserInfo?retryWrites=true&w=majority"
 }
 db = MongoEngine(app)
 app.config['SECRET_KEY'] = 'a0bgF^P(m*jy^iQ$jntTCxLp)raqqpAG'
-app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'static/uploads/..')
+app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'static\\uploads\\')
 app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
